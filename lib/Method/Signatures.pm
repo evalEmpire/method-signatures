396dd17c0c440a83ea34f17f572973e4512520f5
Michael G. Schwern <schwern@pobox.com>
2011-03-22 00:39:28 +1100

    Version 20110322.0027_001

--- lib/Method/Signatures.pm
+++ lib/Method/Signatures.pm
@@ -7,7 +7,7 @@ use base 'Devel::Declare::MethodInstaller::Simple';
 use Method::Signatures::Parser;
 use Data::Alias;
 
-our $VERSION = '20110216.1153_01';
+our $VERSION = '20110322.0027_001';
 
 our $DEBUG = $ENV{METHOD_SIGNATURES_DEBUG} || 0;
 
