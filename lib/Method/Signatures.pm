3557dfd9e2db7233eef40f9b94d444fb70d29c12
Buddy Burden <barefootcoder@gmail.com>
2016-05-16 20:35:35 -0700

    packaging for CPAN: 20160516.2032_001

--- lib/Method/Signatures.pm
+++ lib/Method/Signatures.pm
@@ -9,7 +9,7 @@ use Method::Signatures::Utils;
 use Method::Signatures::Parameter;
 use Method::Signatures::Signature;
 
-our $VERSION = '20160315';
+our $VERSION = '20160516.2032_001';
 
 our $DEBUG = $ENV{METHOD_SIGNATURES_DEBUG} || 0;
 
